48debf74b137a1910b77381502f17b05f2046900
moonelysian <dandyblack@naver.com>
2019-02-22 14:18:35 +0900

    settings.py changed

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '=k4att13)$44snm7jl8gwj7jqww9de-7cialaw5$_71rl#0wsb'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '=k4att13)$44snm7jl8gwj7jqww9de-7cialaw5$_71rl#0wsb')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
